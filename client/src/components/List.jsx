e06207a8fdc7cda3e725e88f6485d56cbe43192a
Henry Neale <hackerhneale@gmail.com>
2018-01-11 14:45:18 -0500

    Implement searching

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ListItem from './ListItem.jsx';
 import { Grid, Image } from 'semantic-ui-react';
 
-const List = props => (
+const List = (props) => (
   <div>
     <h4> Recipes </h4>
     There are {props.items.length} items.
