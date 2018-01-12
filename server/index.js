23709a8d0c92901a55ae503585aec461dbf6b3cd
toowan <mail@toowan.me>
2018-01-11 19:23:27 -0800

    (routes) Add post request handler to server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes/index');
+const database = require('../database/index');
 const app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/', routes);
 
 
 // For componentDidMount - (possibility)
@@ -19,6 +18,31 @@ app.use('/', routes);
 //     }
 //   });
 // });
+
+app.post('/recipes', (req, res, next) => {
+  let keywords = req.body.keywords;
+  let budget = req.body.budget;
+
+  database.getRecipesFromMongo(budget, keywords, (data) => {
+    if (data.length >= 12) {
+      console.log('there are more than 12 results.')
+      res.status(201).json(data);
+    } else {
+      console.log('getting data from API')
+      database.getDataFromAPI(keywords, (apidata) => {
+        database.saveRecipesToMongo(apidata, () => {
+          console.log('getting data from database')
+          database.getRecipesFromMongo(budget, keywords, (data) => {
+            console.log('found data in database')
+            res.status(201).json(data);
+          });
+        });
+      });
+    }
+  });
+
+});
+
 
 
 let PORT = process.env.PORT || 3000;
